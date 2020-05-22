8dc1153f1af107cc9467f83169f465094d014cc8
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-05-21 23:32:29 -0300

    Criando Tabela de gastos

---
+++ src/database/migrations/1590113484611-CreateExpenses.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateExpenses1590113484611 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'expenses',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('expenses');
+  }
+}
